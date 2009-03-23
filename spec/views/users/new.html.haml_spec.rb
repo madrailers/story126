796873c48912a319f2868014622b5dd3e95091f9
bendycode <stephen@bendycode.com>
2009-03-23 09:19:08 -0500

    Minor formatting changes only.

--- spec/views/users/new.html.haml_spec.rb
+++ spec/views/users/new.html.haml_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/users/new.html.haml" do
   include UsersHelper
-  
+
   before(:each) do
     assigns[:user] = stub_model(User,
       :new_record? => true,
@@ -18,7 +18,7 @@ describe "/users/new.html.haml" do
 
   it "renders new user form" do
     render
-    
+
     response.should have_tag("form[action=?][method=post]", users_path) do
       with_tag("input#user_login[name=?]", "user[login]")
       with_tag("input#user_password[name=?]", "user[password]")
@@ -26,5 +26,3 @@ describe "/users/new.html.haml" do
     end
   end
 end
-
-
