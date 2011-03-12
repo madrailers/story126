18d2b564e88cc5077e18a57600f3855ec2b4618b
Mark Anderson and Bradley Grzesiak <dev+doodlehaus+listrophy@bendyworks.com>
2011-03-12 09:56:37 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Story126::Application.config.secret_token = '9ad76cc36b5afc62f0f8960365689ac793a79d638ab4c917ebbdd03194ed536abb02ee55cad9cd31b0f4c4d66b6334e2b05f1805a86253e9d87d3d0b4003ea8f'
